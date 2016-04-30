5b9a6f069af28ac0f42862eeb26a9dd90c3be289
kaylant <kaylant@gmail.com>
2016-04-30 16:38:14 -0500

    while loops in swift

---
+++ whileLoop.playground/Contents.swift
@@ -0,0 +1,55 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Basic While loop
+
+//var i = 1
+//
+//while i < 10 {
+//    
+//    i += 1
+//    
+//    print(i)
+//
+//}
+
+
+//var i = 1
+//
+//while i <= 10 {
+//    
+//    print(i * 5)
+//    
+//    i += 1
+//    
+//}
+
+// can also loop through arrays
+
+//var arr = [8, 3, 1, 9, 4, 5, 7]
+//
+//var i = 0
+//
+//while i < arr.count {
+//    
+//    print(arr[i])
+//    
+//    i += 1
+//    
+//}
+
+// create an array of 7 numbers and subtract one from each of them
+
+var myArr = [2,4,6,7,8,5,3]
+
+var i = 0
+
+while i < myArr.count {
+    
+    print(myArr[i]-1)
+    i += 1
+    
+}
+
+
